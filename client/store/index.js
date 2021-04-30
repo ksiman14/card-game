817be59bf1eb31777794477b5605c45d72b9b1b7
Kelsey Siman <ksiman14@gmail.com>
2021-04-30 18:05:43 -0400

    refactor: removed redux logger

--- client/store/index.js
+++ client/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
+//import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import cards from './cards';
@@ -11,9 +11,7 @@ const reducer = combineReducers({
   currentCard,
   currentStack,
 });
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
-);
+const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware));
 const store = createStore(reducer, middleware);
 
 export default store;
