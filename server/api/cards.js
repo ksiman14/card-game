93f115c399921af5ad6f604fbc3fa80956eda91a
Kelsey Siman <ksiman14@gmail.com>
2021-04-30 11:38:02 -0400

    feat: added express sessions

--- server/api/cards.js
+++ server/api/cards.js
@@ -7,15 +7,18 @@ module.exports = router;
 //api/cards
 router.get('/', async (req, res, next) => {
   try {
-    const cards = await Card.findAll();
-    const hand = [cards[Math.floor(Math.random() * 52)]];
-    cards.forEach((card) => {
-      if (!hand.includes(card)) {
-        const idx = Math.floor(Math.random() * hand.length);
-        hand.splice(idx, 0, card);
-      }
-    });
-    res.json(hand);
+    if (!req.session.hand) {
+      const cards = await Card.findAll();
+      const hand = [cards[Math.floor(Math.random() * 52)]];
+      cards.forEach((card) => {
+        if (!hand.includes(card)) {
+          const idx = Math.floor(Math.random() * hand.length);
+          hand.splice(idx, 0, card);
+        }
+      });
+      req.session.hand = hand;
+    }
+    res.json(req.session.hand);
   } catch (err) {
     next(err);
   }
