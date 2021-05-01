69a385687da30a3e8ba8ed6241efc8ff6a4371d1
Kelsey Siman <ksiman14@gmail.com>
2021-05-01 17:14:59 -0400

    feat: animated lost page

--- client/components/Lost.js
+++ client/components/Lost.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import anime from 'animejs/lib/anime.es';
+import anime, { AnimeInstance } from 'animejs/lib/anime.es';
 
 const Lost = (props) => {
+  const animeRef = React.useRef(null);
+
+  React.useEffect(
+    () =>
+      (animeRef.current = anime({
+        targets: '#sad_face',
+        duration: 1000,
+        loop: true,
+        rotate: 360,
+        elasticity: 600,
+        easing: 'easeOutElastic',
+        delay: function (el, index) {
+          return index * 80;
+        },
+      }))
+  );
+
   return (
     <div id="lost">
       <p>Sorry, you lost...</p>
-      <img src="crying_face.png" />
+      <img id="sad_face" src="crying_face.png" />
       <button type="button" onClick={props.handleClick}>
         Play Again!
       </button>
